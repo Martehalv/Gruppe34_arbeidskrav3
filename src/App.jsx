ba73eb34c547d305c7c77e05512e0f02409c4420
Martehalv <martehal@hiof.no>
2025-04-08 16:02:18 +0200

    satt opp react og sanity + jobbet litt med meny

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import "./styles/Layout.scss";
+import { Link } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <header className="menu">
+        <Link to="/" className="team">
+          TEAM 34
+        </Link>
+        <nav className="navList">
+          <Link to="/">Hjem</Link>
+          <span>|</span>
+          <Link to="/">Celina</Link>
+          <Link to="/">Sara</Link>
+          <Link to="/">Aynazar</Link>
+          <Link to="/">Marte</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <h2>Gruppemedlemmer</h2>
+      <section></section>
+
+      <h2>Arbeidslogg</h2>
+      <section></section>
+    </>
+  );
+}
