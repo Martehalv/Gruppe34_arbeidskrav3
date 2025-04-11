e8f8032e4a8aa9b638091dc9942963091d2214a9
Martehalv <martehal@hiof.no>
2025-04-11 12:08:17 +0200

    jobbet med main og css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,32 @@
 // Home.jsx
 export default function Home() {
   return (
+    //Må bytte ut innholde med dynamisk kode i artiklene!
     <>
       <main>
         <h2>Gruppemedlemmer</h2>
-        <section></section>
+        <section id="groupmembers">
+          <article>
+            <img src="https://cdn.pixabay.com/photo/2021/09/18/15/59/boat-8219886_960_720.jpg" />
+            <p>Navn</p>
+            <p>Email</p>
+          </article>
+          <article>
+            <img src="https://pixabay.com/photos/boat-water-rope-lake-wooden-boat-8219886/" />
+            <p>Navn</p>
+            <p>Email</p>
+          </article>
+          <article>
+            <img src="https://pixabay.com/photos/boat-water-rope-lake-wooden-boat-8219886/" />
+            <p>Navn</p>
+            <p>Email</p>
+          </article>
+          <article>
+            <img src="https://pixabay.com/photos/boat-water-rope-lake-wooden-boat-8219886/" />
+            <p>Navn</p>
+            <p>Email</p>
+          </article>
+        </section>
 
         <h2>Arbeidslogg</h2>
         <section></section>
