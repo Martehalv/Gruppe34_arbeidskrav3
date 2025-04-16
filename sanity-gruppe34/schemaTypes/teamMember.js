742fd83ffe5c35c4beccb45613f1ce6b914cfe94
Martehalv <martehal@hiof.no>
2025-04-16 11:54:10 +0200

    fikset litt feilmeldinger

--- sanity-gruppe34/schemaTypes/teamMember.js
+++ sanity-gruppe34/schemaTypes/teamMember.js
@@ -1,12 +1,12 @@
 export const teamMember = {
-  name: "teamMember",
-  title: "Group member",
-  type: "document",
+  name: 'teamMember',
+  title: 'Group member',
+  type: 'document',
   fields: [
     {
-      name: "name",
-      title: "Name",
-      type: "string"
+      name: 'name',
+      title: 'Name',
+      type: 'string',
     },
     {
       name: 'slug',
@@ -14,13 +14,11 @@ export const teamMember = {
       type: 'slug',
       options: {
         source: 'name',
-        slugify: input =>
+        slugify: (input) =>
           input
             .toLowerCase()
             .replace(/\s+/g, '-') //Mellomrom om til bindestrek
-            .replace(/[åæø]/g, char => (
-              { 'å': 'a', 'ø': 'o', 'æ': 'e' }[char] || char
-            ))
+            .replace(/[åæø]/g, (char) => ({å: 'a', ø: 'o', æ: 'e'})[char] || char)
             .replace(/[^a-z0-9-]/g, '') //Fjerne rare tegn
             .slice(0, 50),
         maxLength: 50,
@@ -45,7 +43,7 @@ export const teamMember = {
       name: 'interests',
       type: 'array',
       title: 'Interests',
-      of: [{ type: 'string' }],
+      of: [{type: 'string'}],
     },
-  ]
+  ],
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,13 @@
+import { client } from "../sanityClient";
+
 export default function Home() {
   return (
-    //Må bytte ut innholde med dynamisk kode i artiklene!
     <>
       <main>
         <h2>Gruppemedlemmer</h2>
         <section id="groupmembers">
           <article>
-            <img src="https://cdn.pixabay.com/photo/2021/09/18/15/59/boat-8219886_960_720.jpg" />
+            <img src="https://pixabay.com/photos/boat-water-rope-lake-wooden-boat-8219886/" />
             <p>Navn</p>
             <p>Email</p>
           </article>
