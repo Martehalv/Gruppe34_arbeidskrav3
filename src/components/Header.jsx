2142306dda62b2d15737c0dc89f8e684f4a6f754
saraamel <saraamelb@icloud.com>
2025-04-20 15:25:13 +0200

    sara
    fikset nav til sanity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,35 @@
-// Header.jsx
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { client } from "../sanityClient";
 import "../styles/Layout.scss";
 
 export default function Header() {
+  const [members, setMembers] = useState([]);
+
+  useEffect(() => {
+    const query = `*[_type == "teamMember"]{
+      name,
+      "slug": slug.current
+    }`;
+
+    client.fetch(query).then((data) => {
+      setMembers(data);
+    });
+  }, []);
+
   return (
     <header className="menu">
       <Link to="/" className="team">TEAM 34</Link>
       <nav className="navList">
         <Link to="/">Hjem</Link>
         <span>|</span>
-        <Link to="/celina">Celina</Link>
-        <Link to="/sara">Sara</Link>
-        <Link to="/aynazar">Aynazar</Link>
-        <Link to="/marte">Marte</Link>
+        {members.map((member, index) => (
+          <span key={member.slug}>
+            <Link to={`/team/${member.slug}`}>{member.name}</Link>
+            {index < members.length - 1 && <span> | </span>}
+          </span>
+        ))}
       </nav>
-    </header> 
+    </header>
   );
 }
